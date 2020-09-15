8e076baed99753f370a924667741fc24117d4176
g-mayer <g-mayer@berkeley.edu>
2020-09-14 19:49:32 -0700

    Added Dog facts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+  const [items, setItems] = useState([]);
+  const changeFact = () => {
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    }
+    const item = items[0].facts[getRandomInt(0, 101)];
+    document.getElementById("fact").innerHTML = item;
+  };
+  const changeCatFact = () => {
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    }
+    const item = items[1].all[getRandomInt(0, 257)];
+    document.getElementById("catFact").innerHTML = item.text;
+  };
+  const fetchItems = async () => {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const catUrl = "https://cat-fact.herokuapp.com/facts?number=100";
+    const dogUrl = "http://dog-api.kinduff.com/api/facts?number=256";
+
+    const catData = await fetch(proxyUrl + catUrl);
+    const dogData = await fetch(proxyUrl + dogUrl);
+
+    const dogFacts = await dogData.json();
+    const catFacts = await catData.json();
+    // setItems(items);
+    const data = [dogFacts, catFacts]
+    setItems(data)
+    console.log("the data has been set")
+  };
+
   return (
+    <>
+    <center><h1>CATS&dogs</h1></center>
+    <div className="facts-container">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h2>Here's the Dog fact</h2>
+      </center>
+      <pre></pre>
+      <div id="fact" className="facts"></div>
+      <button id="bigbutton" onClick={changeFact}>
+        <h3>Change the Fact</h3>
+      </button>
     </div>
+    <div className="App">
+      <center>
+        <h2>Here's the Cat fact</h2>
+      </center>
+      <pre></pre>
+      <div id="catFact" className="facts"></div>
+      <button id="bigbutton" onClick={changeCatFact}>
+        <h3>Change the Fact</h3>
+      </button>
+    </div>
+    </div>
+   
+  </>
   );
 }
 
